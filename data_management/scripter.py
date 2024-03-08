ce9dc742bef4eeb33254ad6a1d915e42c0a56593
Gregor Hartl Watters <mario.garwh@gmail.com>
2024-03-08 04:07:13 +0100

    Few changes here, most in greglib. I did add scripter.py though, for writing out a fat shell script to run all my data generation simulations at once.

---
+++ data_management/scripter.py
@@ -0,0 +1,18 @@
+import math
+
+masses = {1.0: 1.25, 10.0: 12.5, 100.0: 125.0, 1000.0: 1250.0}
+
+evolutions = 60
+epochs = 1_000
+iterations = 1_000_000
+num_mass_means = evolutions # so that each velocity scaling factor has a large mass range
+starting_mass = 1.0
+mass_step = 1
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
